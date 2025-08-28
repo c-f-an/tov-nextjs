53e8c12898dc8a1f85f6104f82adf43369fb717a
hywkd3 <58063144+hywkd3@users.noreply.github.com>
2025-08-28 18:13:24 +0900

    캐싱 처리 추가

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,20 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true
+  },
+  compress: true, // gzip 압축 활성화
+  async headers() {
+    return [
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      }
+    ]
   }
 };
 
