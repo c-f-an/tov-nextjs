4212ed7b1c1c6f66da81521f8436172e39d94eb4
hywkd3 <58063144+hywkd3@users.noreply.github.com>
2025-08-29 00:54:52 +0900

    빌드 에러 수정

--- src/core/domain/repositories/ISponsorRepository.ts
+++ src/core/domain/repositories/ISponsorRepository.ts
@@ -1,9 +1,11 @@
 import { Sponsor } from '../entities/Sponsor';
 
-export interface ISponsorRepository {
+interface ISponsorRepository {
   save(sponsor: Sponsor): Promise<Sponsor>;
   findById(id: number): Promise<Sponsor | null>;
   findByUserId(userId: number): Promise<Sponsor[]>;
   findAll(): Promise<Sponsor[]>;
   delete(id: number): Promise<void>;
 }
+
+export type { ISponsorRepository };
