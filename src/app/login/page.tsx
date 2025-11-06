881cd6ad1d0c5f5d60d16c3223914c530d324fd1
hywkd3 <58063144+hywkd3@users.noreply.github.com>
2025-11-06 16:41:10 +0900

    소셜로그인 막아두기

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -260,7 +260,7 @@ function LoginForm() {
             </div>
           )}
 
-          <div className="mt-6">
+          {/* <div className="mt-6">
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300" />
@@ -327,7 +327,7 @@ function LoginForm() {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
